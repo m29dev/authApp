58882506a2a17cf1738a5667a68036bdda154db5
m29dev <123384597+m29dev@users.noreply.github.com>
2023-05-25 18:18:29 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect(process.env.MONGO_URI)
+        const connection = await mongoose.connect('mongodb+srv://admin:mdb2023@clusterapp.vktihlt.mongodb.net/ReactJwt?retryWrites=true&w=majority')
         if (connection) console.log('db has been connected')
     } catch (err) {
         console.log(err)
